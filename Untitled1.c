7133525795c8e2acc594c2e978c29f8cc5c02413
lProDi <mister-alex2@hotmail.com>
2016-05-10 17:57:19 +0200

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+typedef struct{
+    char codigo_jugador[2];
+    char codigo_plantilla[3];
+}jugador;
+
+int main(){
+int *v,i;
+
+v= (int *) malloc(1,sizeof(int));
+
+for(i=0;i++;)
+
+
+
+
+
+
+
+return 0;
+}
