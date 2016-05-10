5c56c15a72e06de530787801a404f8fb0c4874cd
PatricioFernandez <patriciofernandezflorez@gmail.com>
2016-05-10 17:35:54 +0200

    Add files via upload

---
+++ fichero.h
@@ -0,0 +1,11 @@
+#ifndef FICHERO_H
+#define	FICHERO_H
+
+typedef struct usuario;
+typedef struct equipo;
+typedef struct jugador;
+typedef struct plantilla;
+typedef struct jug_plant;
+
+#endif	/* FICHERO_H */
+
