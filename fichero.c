8efbbb6c60c0193cf038d130aec13e945f378542
JoseJavierGomezRosado <gomezrosadojosejavier@gmail.com>
2016-05-11 11:57:16 +0200

    Add files via upload

--- fichero.c
+++ fichero.c
@@ -2,38 +2,71 @@
 #include <string.h>
 #include <stdio.h>
 
-// TIPOS
 
-typedef struct{
-    char codigo[2];
-    char nombre[20];
-    char tipo[13];
-    char login[5];
-    char pass[5];
-}usuario;
+// CONSTANTES
 
-typedef struct{
-    char codigo[2];
-    char nombre[20];
-}equipo;
+FILE *FICHERO_USUARIO;
+FILE *FICHERO_EQUIPO;
+FILE *FICHERO_JUGADOR;
+FILE *FICHERO_PLANTILLA;
+FILE *FICHERO_JUG_PLAN;
 
-typedef struct{
-    char codigo[2];
-    char codigo_equipo[2];
-    char nombre[20];
-    int precio;
-    int valoracion;
-}jugador;
+char *DATOS_USUARIO;
+char *DATOS_EQUIPO;
+char *DATOS_JUGADOR;
+char *DATOS_PLANTILLA;
+char *DATOS_JUG_PLAN;
 
-typedef struct{
-    char codigo_usuario[2];
-    char codigo[3];
-    char nombre[30];
-    int presupuesto;
-    int puntuacion;
-}plantilla;
+// FUNCIONES INTERNAS
 
-typedef struct{
-    char codigo_jugador[2];
-    char codigo_plantilla[3];
-}jugador;
+static int abrirFicheros(){
+    int res = 0;
+    FICHERO_USUARIO = fopen("usuarios.txt", "w");
+    FICHERO_EQUIPO = fopen("equipos.txt", "w");
+    FICHERO_JUGADOR = fopen("futbolistas.txt", "w");
+    FICHERO_PLANTILLA = fopen("plantillas.txt", "w");
+    FICHERO_JUG_PLAN = fopen("jugadores_plantillas.txt", "w");
+    if(FICHERO_USUARIO == NULL) res = 1;
+    if(FICHERO_EQUIPO == NULL) res = 1;
+    if(FICHERO_JUGADOR == NULL) res = 1;
+    if(FICHERO_PLANTILLA == NULL) res = 1;
+    if(FICHERO_JUG_PLAN == NULL) res = 1;
+    return res;
+}
+
+static void cargarDatos(){
+    cargarDato(FICHERO_USUARIO, DATOS_USUARIO);
+    cargarDato(FICHERO_EQUIPO, DATOS_EQUIPO);
+    cargarDato(FICHERO_JUGADOR, DATOS_JUGADOR);
+    cargarDato(FICHERO_PLANTILLA, DATOS_PLANTILLA);
+    cargarDato(FICHERO_JUG_PLAN, DATOS_JUG_PLAN);
+}
+
+static void cargarDato(FILE f, char* datos){
+    char c;
+    int res = 0;
+    int contador = 0;
+    datos = (char*) malloc(sizeof(char));
+    while((c = getc(f)) != EOF){
+        contador++;
+        datos = (char*) realloc(sizeof(char)*contador);
+        daots[contador-1] = c;
+    }
+}
+
+static void cerrarFicheros(){
+    
+    // ANTES DE CERRAR GUARDAR LOS CAMBIOS
+    fclose(FICHERO_USUARIO);
+    fclose(FICHERO_EQUIPO);
+    fclose(FICHERO_JUGADOR);
+    fclose(FICHERO_PLANTILLA);
+    fclose(FICHERO_JUG_PLAN);
+}
+
+// FUNCIONES PUBLICAS
+
+int empezar(){
+    int res = abrirFicheros();
+    return res;
+}
--- fichero.h
+++ fichero.h
@@ -1,11 +1,41 @@
 #ifndef FICHERO_H
 #define	FICHERO_H
 
-typedef struct usuario;
-typedef struct equipo;
-typedef struct jugador;
-typedef struct plantilla;
-typedef struct jug_plant;
+// TIPOS
+typedef struct{
+    char codigo[2];
+    char nombre[20];
+    char tipo[13];
+    char login[5];
+    char pass[5];
+}usuario;
+
+typedef struct{
+    char codigo[2];
+    char nombre[20];
+}equipo;
+
+typedef struct{
+    char codigo[2];
+    char codigo_equipo[2];
+    char nombre[20];
+    int precio;
+    int valoracion;
+}jugador;
+
+typedef struct{
+    char codigo_usuario[2];
+    char codigo[3];
+    char nombre[30];
+    int presupuesto;
+    int puntuacion;
+}plantilla;
+
+typedef struct{
+    char codigo_jugador[2];
+    char codigo_plantilla[3];
+}jug_plan;
+
+int empezar();
 
 #endif	/* FICHERO_H */
-
