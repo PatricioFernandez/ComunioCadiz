a31521d0d28f219265aa5ee18f84663298021e26
PatricioFernandez <patriciofernandezflorez@gmail.com>
2016-05-24 17:49:51 +0200

    Add files via upload

--- usuario.c
+++ usuario.c
@@ -3,6 +3,36 @@
 #include "usuario.h"
 #include "fichero.h"
 
+
+void tasar(){
+}
+
+void imprimirListaJugadores(Jugador *jugadores,int tamano){
+
+    int contador,contadorEq,tamanoequipos;
+    Equipo *equipos=obtenerEquipos();
+    tamanoequipos=nEquipos();
+    int codigo_equipo;
+    puts("---JUGADORES---");
+    puts("Codigo Equipo Nombre Precio Valoracion");
+    for(contador=0;contador<tamano;contador++){
+
+        printf("%s ",jugadores[contador].codigo);
+        codigo_equipo=jugadores[contador].codigo_equipo;
+            for(contadorEq=0;contadorEq<tamanoequipos;contadorEq++)
+            {
+                if(strcmp(codigo_equipo,equipos[contadorEq].codigo)==0)
+                        printf("%s ",equipos[contadorEq].nombre);
+            }
+        contadorEq=0;
+        printf("%s ",jugadores[contador].nombre);
+        printf("%i ",jugadores[contador].precio);
+        printf("%i ",jugadores[contador].valoracion);
+        puts("");
+    }
+
+}
+
 void crearPlantilla()
 {
     char nombre[60];
@@ -11,13 +41,36 @@ void crearPlantilla()
 
     Plantilla *plantillas=obtenerPlantillas();
     int numero= nPlantillas();
-    printf("%i",numero);
 
-    int contador;
-    for (contador=0;contador<numero;contador++)
+    char codigoPlantilla[3];
+    char numeroEnChar=numero+49;
+
+
+    if(numero+1>10 && numero+1<100)
     {
-        printf("%s\n CONTADOR %i",plantillas[contador].nombre,contador);
+         strcpy(codigoPlantilla,"0");
+
+    }
+    else if(numero+1>100)
+    {
+         strcpy(codigoPlantilla,"");
+    }
+    else{
+         strcpy(codigoPlantilla,"00");
+         codigoPlantilla[2]=numeroEnChar;
     }
 
 
+
+    strcpy(plantillas[numero+1].codigo_usuario,"01");
+    strcpy(plantillas[numero+1].codigo_usuario,codigoPlantilla);
+    strcpy(plantillas[numero+1].nombre,nombre);
+
+    Jugador *jugadores=obtenerJugadores();
+    int numeroJugadores=nJugadores();
+    imprimirListaJugadores(jugadores,numeroJugadores);
+
 }
+
+
+
