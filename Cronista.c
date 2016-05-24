34755140f5c766dac2378ac455f9f9c898c2e679
lProDi <mister-alex2@hotmail.com>
2016-05-24 17:53:17 +0200

    Add files via upload

--- Cronista.c
+++ Cronista.c
@@ -9,25 +9,48 @@ n=nEquipos();
 
 for(i=0;i<n;i++){
     printf("Equipo %s:",equipos[i].codigo);
-    printf("%s",equipos[i].nombre);
+    printf("%s\n",equipos[i].nombre);
 }
 }
 
 void valorarEquipos(Equipo *equipos,Jugador *jugadores){
-int i=0,n,t,j=0;
+int i=0,n,t,p,valor;
+char j[3],c[3],r;
 n=nEquipos();
 t=nJugadores();
-while(i<n && j<t){
-
-
+for(i=0;i<n;i++){
+    printf("Equipo %s:",equipos[i].codigo);
+    printf("%s\n",equipos[i].nombre);
+}
+printf("Introduzca el codigo del equipo a valorar:");
+gets(j);
+for(p=0;p<t;){
+    if(strcmp(jugadores[p].codigo_equipo,j)==0){
+    printf("%s %s %d\n",jugadores[p].codigo,jugadores[p].nombre,jugadores[p].valoracion);
+    p++;
+}else{
+p++;
 
 }
-
-
-
-
-
-
-
-
+}
+do{
+printf("\nIntroduzca el codigo del jugador,al que desea modificar la valoracion:");
+fflush(stdin);
+gets(c);
+for(p=0;p<t;){
+if(strcmp(jugadores[p].codigo_equipo,j)==0 && strcmp(jugadores[p].codigo,c)==0){
+printf("Introduzca la nueva valoracion:");
+scanf("%d",&valor);
+jugadores[p].valoracion=jugadores[p].valoracion+valor;
+//Insertar Nuevo Modulo de GUARDARDATOS//
+printf("%s %s %d\n",jugadores[p].codigo,jugadores[p].nombre,jugadores[p].valoracion);
+p++;
+}else{
+p++;
+}
+}
+fflush(stdin);
+printf("¿Quieres valorar a otro jugador,S/N?");
+r=getchar();
+}while(r!='N');
 }
