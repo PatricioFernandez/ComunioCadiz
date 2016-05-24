cdaff8290e68b3574d556c19000323d4872fbdb0
PatricioFernandez <patriciofernandezflorez@gmail.com>
2016-05-24 16:44:32 +0200

    Add files via upload

--- usuario.c
+++ usuario.c
@@ -8,7 +8,16 @@ void crearPlantilla()
     char nombre[60];
     puts("¿Como desea llamar a la plantilla?");
     fgets(nombre, sizeof(nombre), stdin);
-    printf("%s",nombre);
+
     Plantilla *plantillas=obtenerPlantillas();
+    int numero= nPlantillas();
+    printf("%i",numero);
+
+    int contador;
+    for (contador=0;contador<numero;contador++)
+    {
+        printf("%s\n CONTADOR %i",plantillas[contador].nombre,contador);
+    }
+
 
 }
