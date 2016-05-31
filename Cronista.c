32418f29dff98a1ee76907ce39af98441fe30e72
lProDi <mister-alex2@hotmail.com>
2016-05-31 18:00:26 +0200

    Add files via upload

--- Cronista.c
+++ Cronista.c
@@ -3,7 +3,23 @@
 #include <stdlib.h>
 #include "fichero.h"
 
-void listarEquipos(Equipo *equipos){
+int ppal(){
+int op;
+Equipo *equipo=obtenerEquipos();
+Jugador *jugadores=obtenerJugadores();
+
+printf("Introduzca la operacion a realizar:\n 1.-Listar Equipos.\n 2.-Valorar Jugadores.\n 3.-Salir del programa.\n");
+scanf("%d",&op);
+if(op==1) listarEquipos2(equipo);
+if (op==2) valorarEquipos(equipo,jugadores);
+
+
+
+
+return 0;
+}
+
+void listarEquipos2(Equipo *equipos){
 int i,n;
 n=nEquipos();
 
@@ -14,6 +30,7 @@ for(i=0;i<n;i++){
 }
 
 void valorarEquipos(Equipo *equipos,Jugador *jugadores){
+fflush(stdin);
 int i=0,n,t,p,valor;
 char j[3],c[3],r;
 n=nEquipos();
@@ -22,18 +39,19 @@ for(i=0;i<n;i++){
     printf("Equipo %s:",equipos[i].codigo);
     printf("%s\n",equipos[i].nombre);
 }
+do{
 printf("Introduzca el codigo del equipo a valorar:");
+fflush(stdin);
 gets(j);
 for(p=0;p<t;){
     if(strcmp(jugadores[p].codigo_equipo,j)==0){
-    printf("%s %s %d\n",jugadores[p].codigo,jugadores[p].nombre,jugadores[p].valoracion);
+    printf("%s %s %d %d\n",jugadores[p].codigo,jugadores[p].nombre,jugadores[p].precio,jugadores[p].valoracion);
     p++;
 }else{
 p++;
 
 }
 }
-do{
 printf("\nIntroduzca el codigo del jugador,al que desea modificar la valoracion:");
 fflush(stdin);
 gets(c);
@@ -42,7 +60,7 @@ if(strcmp(jugadores[p].codigo_equipo,j)==0 && strcmp(jugadores[p].codigo,c)==0){
 printf("Introduzca la nueva valoracion:");
 scanf("%d",&valor);
 jugadores[p].valoracion=jugadores[p].valoracion+valor;
-//Insertar Nuevo Modulo de GUARDARDATOS//
+guardarDatosJugador(jugadores,n);
 printf("%s %s %d\n",jugadores[p].codigo,jugadores[p].nombre,jugadores[p].valoracion);
 p++;
 }else{
