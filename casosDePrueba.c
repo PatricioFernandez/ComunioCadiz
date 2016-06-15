c4dce5848cc138b81b42aa877fb1c2d58ef7b5cb
JoseJavierGomezRosado <gomezrosadojosejavier@gmail.com>
2016-06-15 17:25:03 +0200

    Add files via upload

---
+++ casosDePrueba.c
@@ -0,0 +1,45 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "fichero.h"
+/*
+ 
+ 
+ */
+int main(){
+    
+    
+    puts("### MODULO: fichero, FUNCION: loguear ###");
+    char *cod = loguear("admin","jp1234");
+    if(strcmp(cod, "00") != 0){
+        printf("Usaurio existente, contaseña correcta, resultado: OK\n");
+    }else{
+        printf("Usaurio existente, contaseña correcta, resultado: ERROR\n");
+    }
+    cod = loguear("admin","jp4321");
+    if(strcmp(cod, "00") == 0){
+        printf("Usaurio existente, contaseña erronea, resultado: OK\n");
+    }else{
+        printf("Usaurio existente, contaseña erronea, resultado: ERROR\n");
+    }
+    cod = loguear("croni","jp1234");
+    if(strcmp(cod, "00") != 0){
+        printf("Usaurio existente, contaseña de otro usuario, resultado: OK\n");
+    }else{
+        printf("Usaurio existente, contaseña de otro usuario, resultado: ERROR\n");
+    }
+    cod = loguear("croni","jp1234");
+    if(strcmp(cod, "00") != 0){
+        printf("Usaurio existente, contaseña de otro usuario, resultado: OK\n");
+    }else{
+        printf("Usaurio existente, contaseña de otro usuario, resultado: ERROR\n");
+    }
+    cod = loguear("","");
+    if(strcmp(cod, "00") == 0){
+        printf("Usaurio vacio, contaseña vacia, resultado: OK\n");
+    }else{
+        printf("Usaurio vacio, contaseña vacia, resultado: ERROR\n");
+    }
+    
+    return 0;
+}
